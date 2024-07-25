f3f3a6527305f559a0ebb3098682f7c0ed167564
Angryappleseed <gussutr05@gmail.com>
2024-07-24 20:22:08 -0700

    Separated tasks into tasks.py

--- cogs/general.py
+++ cogs/general.py
@@ -1,4 +1,3 @@
-
 #---------------------GENERAL COMMANDS---------------------#
 
 import aiosqlite
@@ -28,24 +27,11 @@ from helpers.database import(
     update_next_run
     )
 
-
 from helpers.colors import colors
 from helpers.emotes import emotes
 
 
 DATABASE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'database', 'database.db')
-
-# Load the role IDs from config.json
-with open('config.json', 'r') as config_file:
-    config = json.load(config_file)
-
-FRESH_MEAT_ROLE_ID = int(config['fresh_meat_role_id'])
-HEADPATTERS_ROLE_ID = int(config['headpatters_role_id'])
-LIWE_ROLE_ID = int(config['liwe_role_id'])
-PROTECTOR_ROLE_ID = int(config['protector_role_id'])
-GOD_ROLE_ID = int(config['god_role_id'])
-FAILED_VERIFY_ROLE_ID = int(config['failed_verify_role_id']) 
-
 
 
 #--------------------HELP PAGINATION----------------#
@@ -106,15 +92,13 @@ class HelpView(discord.ui.View):
         current_prefix = self.bot.custom_prefixes.get(str(self.ctx.guild.id), '!')
         embed = discord.Embed(
             title = "KiichuBot's Help Menu!",
-            description = "It is HIGHLY recommened to enable developer mode when setting up this bot.",
+            description = "It is HIGHLY recommended to enable developer mode when setting up this bot.",
             color=colors["blue"],   
         )
         embed.add_field(name=f"Current Prefix: `{current_prefix}` | Supports Slash Commands", value="This prefix can be changed with the `setprefix` command.", inline=False)
         embed.add_field(name="About me:", value="""I'm KiichuBot, A multipurpose Discord Bot, made by Angryappleseed (<@484856870725484560>). 
                         Please use the dropdown below for help with specific commands.)""", inline=False)
         return embed
-
-
 
 
 def parse_time_interval(interval: str) -> int:
@@ -135,8 +119,6 @@ def parse_time_interval(interval: str) -> int:
         return amount * 86400
     else:
         raise ValueError("Invalid time unit.")
-
-
 
 
 def format_interval(seconds):
@@ -165,10 +147,10 @@ class General(commands.Cog, name="general"):
         self.bot = bot
         self.cog_descriptions = {
             'general': "Common commands for regular usage.",
+            'tasks': "Commands for background tasks.",
             'owner': "Commands that are reserved for the bot owner(s)."
         }
-        self.min_account_age_days = 7
-        self.log_channel_id = 906624474403717141
+
 
 #---------Automated message loop--------------------#
     @commands.Cog.listener()
@@ -196,95 +178,6 @@ class General(commands.Cog, name="general"):
         await self.bot.wait_until_ready()
 
 
-
-#---------Role update listener--------------------#
-    @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        # Detect if a user receives headpatters or higher roles and remove fresh meat role
-        higher_roles = {HEADPATTERS_ROLE_ID, LIWE_ROLE_ID, PROTECTOR_ROLE_ID, GOD_ROLE_ID}
-        received_higher_role = any(role.id in higher_roles for role in after.roles) and not any(role.id in higher_roles for role in before.roles)
-        if received_higher_role and FRESH_MEAT_ROLE_ID in [role.id for role in before.roles]:
-            fresh_meat_role = discord.utils.get(after.guild.roles, id=FRESH_MEAT_ROLE_ID)
-            if fresh_meat_role:
-                await after.remove_roles(fresh_meat_role)
-
-        # Detect if someone gets fresh meat while having headpatters or higher roles
-        if FRESH_MEAT_ROLE_ID in [role.id for role in after.roles]:
-            if any(role.id in higher_roles for role in after.roles):
-                fresh_meat_role = discord.utils.get(after.guild.roles, id=FRESH_MEAT_ROLE_ID)
-                if fresh_meat_role:
-                    await after.remove_roles(fresh_meat_role)
-
-        # Detect if someone failed verify
-        if FAILED_VERIFY_ROLE_ID in [role.id for role in after.roles] and FAILED_VERIFY_ROLE_ID not in [role.id for role in before.roles]:
-            if any(role.id in higher_roles for role in after.roles):
-                log_channel = self.bot.get_channel(self.log_channel_id)
-                if log_channel:
-                    embed = discord.Embed(
-                        title="Goofball clicked on the anti-bot button while in the server",
-                        description=f"**User: **{after.name} {after.mention}\nUser clicked on the button that tells them NOT TO CLICK ON, but was not kicked due to being headpatters+",
-                        color=colors["gold"],
-                        timestamp=datetime.now()
-                    )
-                    embed.set_footer(text=f"User ID: {after.id}")
-                    await log_channel.send(embed=embed)
-
-                failed_verify_role = discord.utils.get(after.guild.roles, id=FAILED_VERIFY_ROLE_ID)
-                if failed_verify_role:
-                    await after.remove_roles(failed_verify_role)
-            else:
-                log_channel = self.bot.get_channel(self.log_channel_id)
-                if log_channel:
-                    embed = discord.Embed(
-                        title="Potential Bot Kicked",
-                        description=f"**User: **{after.name} {after.mention}\nUser clicked on the button that tells them NOT TO CLICK ON, so they were automatically removed.",
-                        color=colors["red"],
-                        timestamp=datetime.now()
-                    )
-                    embed.set_footer(text=f"User ID: {after.id}")
-                    await log_channel.send(embed=embed)
-
-                failed_verify_role = discord.utils.get(after.guild.roles, id=FAILED_VERIFY_ROLE_ID)
-                if failed_verify_role:
-                    await after.remove_roles(failed_verify_role)
-
-                # Adding a short delay before kicking the user
-                await asyncio.sleep(2)
-
-                await after.kick(reason="Clicked on Bot Deterrent button.")
-
-
-
-#------------NEW ACCOUNT VERIFICATION------------------#
-    @commands.Cog.listener()
-    async def on_member_join(self, member: discord.Member):
-        account_age_days = (datetime.now(timezone.utc) - member.created_at).days
-        if account_age_days < self.min_account_age_days:
-            try:
-                await member.send(
-                    f"Hello {member.name}, your account is suspected to be an alt in {member.guild.name}. "
-                    f"If you believe this to be an error, please feel free to add and DM `angryappleseed` about the issue."
-                )
-            except discord.Forbidden:
-                pass 
-
-            await member.kick(reason=f"Account is too new. Must be at least {self.min_account_age_days} days old.")
-
-            # Log the kick action
-            log_channel = self.bot.get_channel(self.log_channel_id)
-            if log_channel:
-                embed = discord.Embed(
-                    title="Suspected Alt Kicked",
-                    description=f"**User: **{member.name}{member.mention}\nAccount is only {account_age_days} days old. (required age is {self.min_account_age_days} days)",
-                    color=colors["red"],
-                    timestamp=datetime.now()
-                )
-                embed.set_footer(text=f"User ID: {member.id}")
-                await log_channel.send(embed=embed)
-
-
-
-
 #-------------------- HELP------------------------#
 
     @commands.hybrid_command(
@@ -294,11 +187,10 @@ class General(commands.Cog, name="general"):
     @checks.not_blacklisted()
     @checks.is_moderator()
     async def help(self, ctx):
-        included_cogs = ["general", "owner"]
+        included_cogs = ["general", "tasks", "owner"]
         view = HelpView(ctx, included_cogs)
         embed = view.get_homescreen_embed()
         await ctx.send(embed=embed, view=view)
-
 
 
 #---------------------SET PREFIX--------------------#
@@ -392,8 +284,6 @@ class General(commands.Cog, name="general"):
         await channel.send(message)
 
 
-
-
 #-------------------------REMOVE AUTO MESSAGE COMMAND--------------------------#
 
     @commands.hybrid_command(
@@ -408,7 +298,6 @@ class General(commands.Cog, name="general"):
             color=colors["blue"]
         )
         await ctx.send(embed=embed)
-
 
 
 #-------------------------LIST AUTO MESSAGE COMMAND--------------------------#
@@ -426,40 +315,6 @@ class General(commands.Cog, name="general"):
             embed.add_field(name=f"ID: {msg[0]}", value=f"Channel: <#{msg[1]}> - Interval: {formatted_interval}\nMessage: {msg[2]}", inline=False)
         await ctx.send(embed=embed)
 
-
-
-
-#-------------------------REMOVE HEADPATTERS ROLE--------------------------#
-
-    # @commands.hybrid_command(
-    #     name="removeheadpatters",
-    #     description="Removes Headpatters from Legs I Won't Eat."
-    # )
-    # @checks.is_moderator()
-    # async def remove_headpatters(self, ctx: commands.Context):
-    #     guild = ctx.guild
-    #     legs_i_wont_eat_role = guild.get_role(781190734954758154)
-    #     headpatters_role = guild.get_role(752590175711920198)
-
-    #     if not legs_i_wont_eat_role or not headpatters_role:
-    #         embed = discord.Embed(
-    #             description="Could not find the specified roles in the server.",
-    #             color=colors["red"]
-    #         )
-    #         await ctx.send(embed=embed)
-    #         return
-
-    #     removed_count = 0
-    #     for member in guild.members:
-    #         if legs_i_wont_eat_role in member.roles and headpatters_role in member.roles:
-    #             await member.remove_roles(headpatters_role)
-    #             removed_count += 1
-
-    #     embed = discord.Embed(
-    #         description=f"Successfully removed the Headpatters role from {removed_count} users who had the Legs I Won't Eat role.",
-    #         color=colors["blue"]
-    #     )
-    #     await ctx.send(embed=embed)
 
 
 
---
+++ cogs/tasks.py
@@ -0,0 +1,127 @@
+import asyncio
+import json
+
+from datetime import datetime, timezone
+
+import discord
+from discord.ext import commands
+
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+
+from helpers import checks
+from helpers.colors import colors
+from helpers.emotes import emotes
+
+
+# Load the role IDs from config.json
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+FRESH_MEAT_ROLE_ID = int(config['fresh_meat_role_id'])
+HEADPATTERS_ROLE_ID = int(config['headpatters_role_id'])
+LIWE_ROLE_ID = int(config['liwe_role_id'])
+PROTECTOR_ROLE_ID = int(config['protector_role_id'])
+GOD_ROLE_ID = int(config['god_role_id'])
+FAILED_VERIFY_ROLE_ID = int(config['failed_verify_role_id'])
+
+
+class Tasks(commands.Cog, name="tasks"):
+    def __init__(self, bot):
+        self.bot = bot
+        self.min_account_age_days = 7
+        self.log_channel_id = 906624474403717141
+
+
+#---------Role update listener--------------------#
+
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        higher_roles = {HEADPATTERS_ROLE_ID, LIWE_ROLE_ID, PROTECTOR_ROLE_ID, GOD_ROLE_ID}
+
+        # Detect if a user receives headpatters or higher roles and remove fresh meat role
+        received_higher_role = any(role.id in higher_roles for role in after.roles) and not any(role.id in higher_roles for role in before.roles)
+        if received_higher_role and FRESH_MEAT_ROLE_ID in [role.id for role in before.roles]:
+            fresh_meat_role = discord.utils.get(after.guild.roles, id=FRESH_MEAT_ROLE_ID)
+            if fresh_meat_role:
+                await after.remove_roles(fresh_meat_role)
+
+        # Detect if someone gets fresh meat while having headpatters or higher roles
+        if FRESH_MEAT_ROLE_ID in [role.id for role in after.roles]:
+            if any(role.id in higher_roles for role in after.roles):
+                fresh_meat_role = discord.utils.get(after.guild.roles, id=FRESH_MEAT_ROLE_ID)
+                if fresh_meat_role:
+                    await after.remove_roles(fresh_meat_role)
+
+        # Detect if someone failed verify
+        if FAILED_VERIFY_ROLE_ID in [role.id for role in after.roles] and FAILED_VERIFY_ROLE_ID not in [role.id for role in before.roles]:
+            if any(role.id in higher_roles for role in after.roles):
+                log_channel = self.bot.get_channel(self.log_channel_id)
+                if log_channel:
+                    embed = discord.Embed(
+                        title="Goofball clicked on the anti-bot button while in the server",
+                        description=f"**User: **{after.name} {after.mention}\nUser clicked on the button that tells them NOT TO CLICK ON, but was not kicked due to being headpatters+",
+                        color=colors["gold"],
+                        timestamp=datetime.now()
+                    )
+                    embed.set_footer(text=f"User ID: {after.id}")
+                    await log_channel.send(embed=embed)
+
+                failed_verify_role = discord.utils.get(after.guild.roles, id=FAILED_VERIFY_ROLE_ID)
+                if failed_verify_role:
+                    await after.remove_roles(failed_verify_role)
+            else:
+                log_channel = self.bot.get_channel(self.log_channel_id)
+                if log_channel:
+                    embed = discord.Embed(
+                        title="Potential Bot Kicked",
+                        description=f"**User: **{after.name} {after.mention}\nUser clicked on the button that tells them NOT TO CLICK ON, so they were automatically removed.",
+                        color=colors["red"],
+                        timestamp=datetime.now()
+                    )
+                    embed.set_footer(text=f"User ID: {after.id}")
+                    await log_channel.send(embed=embed)
+
+                failed_verify_role = discord.utils.get(after.guild.roles, id=FAILED_VERIFY_ROLE_ID)
+                if failed_verify_role:
+                    await after.remove_roles(failed_verify_role)
+
+                # short delay before kicking the user
+                await asyncio.sleep(2)
+
+                await after.kick(reason="Clicked on Bot Deterrent button.")
+
+
+
+#------------NEW ACCOUNT DETECTION------------------#
+
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        account_age_days = (datetime.now(timezone.utc) - member.created_at).days
+        if account_age_days < self.min_account_age_days:
+            try:
+                await member.send(
+                    f"Hello {member.name}, your account is suspected to be an alt in {member.guild.name}. "
+                    f"If you believe this to be an error, please feel free to add and DM `angryappleseed` about the issue."
+                )
+            except discord.Forbidden:
+                pass 
+
+            await member.kick(reason=f"Account is too new. Must be at least {self.min_account_age_days} days old.")
+
+            # Log the kick action
+            log_channel = self.bot.get_channel(self.log_channel_id)
+            if log_channel:
+                embed = discord.Embed(
+                    title="Suspected Alt Kicked",
+                    description=f"**User: **{member.name}{member.mention}\nAccount is only {account_age_days} days old. (required age is {self.min_account_age_days} days)",
+                    color=colors["red"],
+                    timestamp=datetime.now()
+                )
+                embed.set_footer(text=f"User ID: {member.id}")
+                await log_channel.send(embed=embed)
+
+
+async def setup(bot):
+    await bot.add_cog(Tasks(bot))
